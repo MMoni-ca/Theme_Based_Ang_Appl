43e9818e0e8e25d52d8b350cbdd2c4cca2c00a46
MMoni-ca <masuram.monica@gmail.com>
2024-06-13 21:17:05 +0530

    Theme based Agular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ProductBoard';
+
+  // user settng/preferences API - which will fetch the saved
+  isUserDarkTheme = false;
+
+  toggleMe() {
+    this.isUserDarkTheme = !this.isUserDarkTheme;
+  }
 }
